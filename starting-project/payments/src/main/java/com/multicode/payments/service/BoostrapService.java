8e204af2ce6ceccbd6eb0809f84f6233c8bb1eea
vppmatt <matt@multicode.co.uk>
2023-01-19 08:19:52 +0000

    making data file accessible within jar

--- starting-project/payments/src/main/java/com/multicode/payments/service/BoostrapService.java
+++ starting-project/payments/src/main/java/com/multicode/payments/service/BoostrapService.java
@@ -1,7 +1,6 @@
 package com.multicode.payments.service;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -11,6 +10,8 @@ import java.util.Arrays;
 import com.multicode.payments.data.CreditCardTransactionRepository;
 import com.multicode.payments.domain.CreditCardTransaction;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
@@ -24,27 +25,27 @@ public class BoostrapService {
     @PostConstruct
     public void initData() throws IOException {
         if (repository.count() == 0) {
-            ClassLoader classLoader = getClass().getClassLoader();
-            File file = new File(classLoader.getResource("data.txt").getFile());
-            Path path = Paths.get(file.toURI());
-            Files.lines(path).forEach(line -> {
-                String[] fields = line.replaceAll(" ","").split(",");
-                CreditCardTransaction creditCardTransaction =
-                        new CreditCardTransaction(
-                                Integer.parseInt(fields[0].replace("(","")),
-                                Double.parseDouble(fields[1]),
-                                fields[2].replace("\"","").toLowerCase(),
-                                fields[3].replace("\"",""),
-                                LocalDate.parse(fields[4].replace("\"","")),
-                                fields[5].replace("\"",""),
-                                Integer.parseInt(fields[6]),
-                                Double.parseDouble(fields[7]),
-                                fields[8].replace("\"","").replace(")",""));
-                repository.save(creditCardTransaction);
-            });
-
-
+            try (InputStream inputStream = getClass().getResourceAsStream("/data.txt");
+                 BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+                reader.lines().forEach(line -> {
+                    String[] fields = line.replaceAll(" ", "").split(",");
+                    CreditCardTransaction creditCardTransaction =
+                            new CreditCardTransaction(
+                                    Integer.parseInt(fields[0].replace("(", "")),
+                                    Double.parseDouble(fields[1]),
+                                    fields[2].replace("\"", "").toLowerCase(),
+                                    fields[3].replace("\"", ""),
+                                    LocalDate.parse(fields[4].replace("\"", "")),
+                                    fields[5].replace("\"", ""),
+                                    Integer.parseInt(fields[6]),
+                                    Double.parseDouble(fields[7]),
+                                    fields[8].replace("\"", "").replace(")", ""));
+                    repository.save(creditCardTransaction);
+                });
+            }
         }
-    }
 
+
+    }
 }
+
